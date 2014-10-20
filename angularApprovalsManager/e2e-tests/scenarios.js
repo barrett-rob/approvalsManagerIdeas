441fd3026850cf74d9be4dfc89de1afcea38778c
Rob Barrett <rob.barrett@ventyx.abb.com>
2014-10-20 13:28:07 +1100

    rename view1 to home

--- angularApprovalsManager/e2e-tests/scenarios.js
+++ angularApprovalsManager/e2e-tests/scenarios.js
@@ -6,21 +6,20 @@ describe('my app', function() {
 
   browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  it('should automatically redirect to /home when location hash/fragment is empty', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/home");
   });
 
 
-  describe('view1', function() {
+  describe('home', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html#/home');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    it('should render home view when user navigates to /home', function() {
+      expect(browser.getLocationAbsUrl()).toMatch("/home");
     });
 
   });
