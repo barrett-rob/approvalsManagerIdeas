c468a760296c796a5d47502ae99ea0c700accf79
Rob Barrett <rob.barrett@ventyx.abb.com>
2014-10-21 18:38:51 +1100

    e2e test for settings + some indentation

--- angularApprovalsManager/e2e-tests/scenarios.js
+++ angularApprovalsManager/e2e-tests/scenarios.js
@@ -2,26 +2,36 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('approvalsManager e2e tests', function() {
 
-  browser.get('index.html');
+	browser.get('index.html')
 
-  it('should automatically redirect to /home when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/home");
-  });
+	it('should automatically redirect to /home when location hash/fragment is empty', function() {
+		expect(browser.getLocationAbsUrl()).toMatch("/home")
+	})
 
+    describe('home', function() {
 
-  describe('home', function() {
+        beforeEach(function() {
+            browser.get('index.html#/home')
+        })
 
-    beforeEach(function() {
-      browser.get('index.html#/home');
-    });
+        it('should render home view when user navigates to /home', function() {
+            expect(browser.getLocationAbsUrl()).toMatch("/home")
+        })
 
+    })
 
-    it('should render home view when user navigates to /home', function() {
-      expect(browser.getLocationAbsUrl()).toMatch("/home");
-    });
+    describe('settings', function() {
 
-  });
+        beforeEach(function() {
+            browser.get('index.html#/settings')
+        })
 
-});
+        it('should render settings view when user navigates to /settings', function() {
+            expect(browser.getLocationAbsUrl()).toMatch("/settings")
+        })
+
+    })
+
+})
--- angularApprovalsManager/karma.conf.js
+++ angularApprovalsManager/karma.conf.js
@@ -1,37 +1,37 @@
 module.exports = function(config){
-  config.set({
+	config.set({
 
-  basePath : './',
+	basePath : './',
 
-  files : [
-  'app/bower_components/angular/angular.js',
-  'app/bower_components/angular-route/angular-route.js',
-  'app/bower_components/angular-mocks/angular-mocks.js',
-  'app/x2js/*.js',
-  'app/home/*.js',
-  'app/settings/*.js',
-  'app/ria/*.js',
-  ],
+	files : [
+	'app/bower_components/angular/angular.js',
+	'app/bower_components/angular-route/angular-route.js',
+	'app/bower_components/angular-mocks/angular-mocks.js',
+	'app/x2js/*.js',
+	'app/home/*.js',
+	'app/settings/*.js',
+	'app/ria/*.js',
+	],
 
-  autoWatch : true,
+	autoWatch : true,
 
-  frameworks: ['jasmine'],
+	frameworks: ['jasmine'],
 
-  browsers : ['Chrome'],
-  //browsers : ['Chrome', 'Safari'],
+	browsers : ['Chrome'],
+	//browsers : ['Chrome', 'Safari'],
 
-  plugins : [
-  'karma-chrome-launcher',
-  'karma-safari-launcher',
-  'karma-firefox-launcher',
-  'karma-jasmine',
-  'karma-junit-reporter'
-  ],
+	plugins : [
+	'karma-chrome-launcher',
+	'karma-safari-launcher',
+	'karma-firefox-launcher',
+	'karma-jasmine',
+	'karma-junit-reporter'
+	],
 
-  junitReporter : {
-    outputFile: 'test_out/unit.xml',
-    suite: 'unit'
-  }
+	junitReporter : {
+		outputFile: 'test_out/unit.xml',
+		suite: 'unit'
+	}
 
-  });
+	});
 };
