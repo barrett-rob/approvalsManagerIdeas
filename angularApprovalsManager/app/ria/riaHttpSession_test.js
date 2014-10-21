d2f6dd7d56d172ce61cf3cb58be43f0c42f14f6c
Rob Barrett <rob.barrett@ventyx.abb.com>
2014-10-21 16:12:17 +1100

    started service which will log in

--- angularApprovalsManager/app/ria/riaHttpSession_test.js
+++ angularApprovalsManager/app/ria/riaHttpSession_test.js
@@ -9,12 +9,18 @@ describe('riaHttpSession tests', function() {
 
 	describe('login tests', function() {
 
+		var scope, controller, httpBackend, login
+
+		beforeEach(inject(function(_$controller_, _$httpBackend_, $rootScope, _login_) {
+			scope = $rootScope.$new()
+			controller = _$controller_
+			httpBackend = _$httpBackend_
+			login = _login_
+		}))
+
 		it('login tests...', inject(function(_$controller_, _$httpBackend_, $rootScope, login) {
-			var scope = $rootScope.$new()
 			expect(scope).toBeDefined()
-			var controller = _$controller_
 			expect(controller).toBeDefined()
-			var httpBackend = _$httpBackend_
 			expect(httpBackend).toBeDefined()
 			expect(login).toBeDefined()
 		}))
