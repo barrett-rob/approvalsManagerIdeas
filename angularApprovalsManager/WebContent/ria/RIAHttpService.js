1b13fe02d87fd753d1e6602374d7d0e62f87831b
Rob Barrett <rob.barrett@ventyx.abb.com>
2014-10-23 18:56:22 +1100

    set up interaction object for service action + tests

--- angularApprovalsManager/WebContent/ria/RIAHttpService.js
+++ angularApprovalsManager/WebContent/ria/RIAHttpService.js
@@ -6,8 +6,9 @@
 angular.module('RIAHttpService', [ 'RIAURLService', 'RIACredentialsService' ])
 .config(function() {
 
-	// init connection id
+	// init state
 	self.connectionId = undefined
+	self.x2js = new X2JS()
 
 	// utility methods
 	self.createInteraction = function(
@@ -15,11 +16,12 @@ angular.module('RIAHttpService', [ 'RIAURLService', 'RIACredentialsService' ])
 		actions // array of action (see self.createAction)
 		) {
 		// set up interaction object
-		var interaction = { 
-			'actions': actions, 
-			'application': application, 
-			'applicationPage': undefined
+		var interaction = { 'actions': actions }
+		if (angular.isDefined(self.connectionId)) {
+			interaction.connectionId = self.connectionId
 		}
+		interaction.application = application
+		interaction.applicationPage = undefined // is applicationPage important?
 		return { 'interaction': interaction }
 	}
 	self.createAction = function(
@@ -32,6 +34,27 @@ angular.module('RIAHttpService', [ 'RIAURLService', 'RIACredentialsService' ])
 		}
 		return { 'action': action }
 	}
+	self.createServiceAction = function(
+		service, // string
+		operation, // string
+		returnWarnings, // boolean
+		requiredAttributes, // object
+		restart, // object
+		maxInstances, // int
+		dto // object
+		) {
+		var serviceaction = self.createAction('service', { 
+			'name': service, 
+			'operation': operation, 
+			'returnWarnings': returnWarnings,
+			'dto': dto,
+			'requiredAttributes': requiredAttributes,
+			'restart': restart,
+			'maxInstances': maxInstances,
+			}
+		)
+		return serviceaction
+	}
 	self.getBindUrl = function(url) {
 		return url + '/bind'
 	}
@@ -39,22 +62,68 @@ angular.module('RIAHttpService', [ 'RIAURLService', 'RIACredentialsService' ])
 		var response = { 'messages': { 'errors': { 'message': { 'text': text } } } }
 		return response
 	}
+	self.executePost = function(http, url, interaction, successFunction, errorFunction) {
+		var xml = x2js.json2xml_str(interaction)
+		http.post(url, xml, {'headers': {'Content-Type':'application/xml'} })
+		.success(function(data, status, headers, config) {
+			if (successFunction) {
+				successFunction(data, status, headers, config)
+			}
+		})
+		.error(function(data, status, headers, config) {
+			if (errorFunction) {
+				errorFunction(data, status, headers, config)
+			}
+		})
+	}
 })
 .factory('executeService', [ '$http', 'getUrl', function($http, getUrl) {
-	// executes a service call, calls the 1st callback 
-	// if the login succeeded, or the 2nd if it failed
-	return function(successCallback, failureCallback) {
+	return function(
+		service, // string
+		operation, // string
+		returnWarnings, // boolean
+		requiredAttributes, // object
+		restart, // object
+		maxInstances, // int
+		dto, // object
+		successCallback, // function(response)
+		failureCallback // function(response)
+		) {
+		// executes a service call, calls the 1st callback if
+		// the invocation succeeded, or the 2nd if it failed
+
+		// first check for connectionId
 		if (!angular.isDefined(self.connectionId)) {
 			if (failureCallback) {
 				failureCallback(createErrorResponse('you must log in using executeLogin() before using executeService()'))
 			}
+			return
 		}
+		var serviceaction = self.createServiceAction(
+			service, 
+			operation, 
+			returnWarnings,
+			requiredAttributes,
+			restart,
+			maxInstances,
+			dto
+			)
+		var interaction = self.createInteraction('RIAHttpService.executeService', [ serviceaction ])
+		var url = self.getBindUrl(getUrl())
+		self.executePost($http, url, interaction, function(data, status, headers, config) {
+			// success
+		}, function(data, status, headers, config) {
+			// error
+		})
 	}
 }])
 .factory('executeLogin', [ '$http', 'getCredentials', 'getUrl', function($http, getCredentials, getUrl) {
-	// executes a login, calls the 1st callback if
-	// the login succeeded, or the 2nd if it failed
-	return function(successCallback, failureCallback) {
+	return function(
+		successCallback, // function(response)
+		failureCallback // function(response)
+		) {
+		// executes a login, calls the 1st callback if
+		// the login succeeded, or the 2nd if it failed
 		var credentials = getCredentials()
 		// construct interaction object
 		var data = {
@@ -66,12 +135,9 @@ angular.module('RIAHttpService', [ 'RIAURLService', 'RIACredentialsService' ])
 		}
 		var action = self.createAction('login', data)
 		var interaction = self.createInteraction('login', [ action ])
-		var x2js = new X2JS()
-		var xml = x2js.json2xml_str(interaction)
-		// execute post
 		var url = self.getBindUrl(getUrl())
-		$http.post(url, xml, {'headers': {'Content-Type':'application/xml'} })
-		.success(function(data, status, headers, config) {
+		self.executePost($http, url, interaction, function(data, status, headers, config) {
+			// success
 	  		// populate response object
 			var json = x2js.xml_str2json(data)
 	  		var response = json.interaction.actions.action
@@ -98,9 +164,9 @@ angular.module('RIAHttpService', [ 'RIAURLService', 'RIACredentialsService' ])
 		 			successCallback(response)
 		 		}
 			}
-		})
-		.error(function(data, status, headers, config) {
-			// special case: populate error messages
+		}, function(data, status, headers, config) {
+			// error
+			// comms failure? - populate error messages
 			if (failureCallback) {
 				failureCallback(createErrorResponse('login failed: http status code' + status))
 			}
@@ -109,7 +175,10 @@ angular.module('RIAHttpService', [ 'RIAURLService', 'RIACredentialsService' ])
 }])
 .factory('poke', [ '$http', function($http) {
 	return function(url, pokeCallback) {
-		// perform GET
+		// perform a GET of the supplied url, calls
+		// the supplied function with a boolean that
+		// indicates whether the GET was successful.
+		// (redirections are considered a successful poke)
 		$http.get(url)
 		.success(function(data, status, headers, config) {
 	 		if (pokeCallback) {
