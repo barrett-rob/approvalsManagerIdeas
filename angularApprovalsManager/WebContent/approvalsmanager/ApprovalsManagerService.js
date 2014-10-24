4721f5a848c35f863e9fa0d6cbfe4d263751dc33
Rob Barrett <rob.barrett@ventyx.abb.com>
2014-10-24 14:23:30 +1100

    start impl of items retrieval

--- angularApprovalsManager/WebContent/approvalsmanager/ApprovalsManagerService.js
+++ angularApprovalsManager/WebContent/approvalsmanager/ApprovalsManagerService.js
@@ -29,6 +29,9 @@ angular.module('ApprovalsManagerService', [ 'RIAHttpService' ])
 			100, // max instances
 			dto, // dto
 			function(response) {
+				if (angular.isArray(response)) {
+					throw 'expect only one response, got ' + response.length
+				}
 				var itemTypeCounts = {}
 				if (angular.isDefined(response.data.result.dto)) {
 					if (angular.isArray(response.data.result.dto)) {
@@ -47,6 +50,42 @@ angular.module('ApprovalsManagerService', [ 'RIAHttpService' ])
 				}
 				if (whenComplete) {
 					whenComplete(itemTypeCounts)
+				}
+			}
+		)
+	}
+}])
+.factory('getItems', [ 'executeService', function(executeService) {
+	return function (
+		itemType, // string
+		whenComplete // function(items)
+		) {
+		var dto = angular.copy(self.filters)
+		dto.tran877Type = itemType
+		executeService(
+			'com.mincom.ellipse.service.m3875.approvalsmanager.ApprovalsManagerService',
+			'retrieveApprovals',
+			true, // return warnings
+			undefined, // required attributes
+			undefined, // restart
+			100, // max instances
+			dto, // dto
+			function(response) {
+				if (angular.isArray(response)) {
+					throw 'expect only one response, got ' + response.length
+				}
+				var items = []
+				if (angular.isDefined(response.data.result.dto)) {
+					if (angular.isArray(response.data.result.dto)) {
+						var dtos = response.data.result.dto
+						for (var i in dtos) {
+							var dto = dtos[i]
+							items.push(dto)
+						}
+					}
+				}
+				if (whenComplete) {
+					whenComplete(items)
 				}
 			}
 		)
